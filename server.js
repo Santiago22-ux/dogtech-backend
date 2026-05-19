66cb76036fabe35866be7b00c8f9bb26c792139a
Santi <baironelgrandioso@gmail.com>
2026-05-19 14:59:52 -0500

    Fix: Eliminada ruta estatica duplicada en server

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 // Middlewares necesarios para entender JSON y permitir conexión con React
 app.use(cors()); 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const USERS_FILE = path.join(__dirname, 'users.json');
