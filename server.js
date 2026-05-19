4237edaca0a3385747991f6e0cb1226978cf134a
Santi <baironelgrandioso@gmail.com>
2026-05-19 14:55:03 -0500

    Moviendo frontend a carpeta public para Render

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const USERS_FILE = path.join(__dirname, 'users.json');
 
